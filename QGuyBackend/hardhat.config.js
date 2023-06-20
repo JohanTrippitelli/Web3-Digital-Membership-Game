8080fa98c1f0537c689fbcd9e48410c8a19944f4
7thdeadlysinE <jjtrippiered@outlook.com>
2023-06-20 15:24:52 +0200

    Addition of Sepolia and Goerli for testnet deployment

--- QGuyBackend/hardhat.config.js
+++ QGuyBackend/hardhat.config.js
@@ -9,9 +9,11 @@ require("hardhat-deploy");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
+const S_API_URL = process.env.S_API_URL || "";
+const G_API_URL = process.env.G_API_URL || "";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const S_PRIVATE_KEY = process.env.S_PRIVATE_KEY || "";
+const G_PRIVATE_KEY = process.env.G_PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
@@ -20,6 +22,14 @@ module.exports = {
     hardhat: {
       chainId: 31337,
       // gasPrice: 130000000000,
+    },
+    sepolia: {
+      url: S_API_URL,
+      accounts: [`0x${S_PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: G_API_URL,
+      accounts: [`0x${G_PRIVATE_KEY}`],
     },
     // mainnet: {
     //   url: process.env.MAINNET_RPC_URL,
